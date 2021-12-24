b751ddaca268e49e54fa381e390dd46c173e980d
용주 곽 <amese1225@gmail.com>
2021-12-24 23:19:33 +0900

    <=> 연산자

--- cpp20/cpp20/cpp20.cpp
+++ cpp20/cpp20/cpp20.cpp
@@ -8,131 +8,84 @@ using namespace std;
 #include <concepts>
 #include <coroutine>
 #include "MyCoroutine.h"
-// 오늘의 주제 : Corutine
+// 오늘의 주제 : Three-Way Comparison 연산자 (Spaceship operator <=>)
 
-
-// 유니티 = 코루틴
-// - c#에서 있는  코루틴 문법을 유니티에서 적극적으로 채용해서 제공
-
-
-struct CoroutineObject
+struct MyInt
 {
-	// 1) promise 객체는 다음과 같은 인터페이스를 제공해야 함
-	// - 기본 생성자 : promise 객체는 기본 생성자로 만들어질 수 있어야 함
-	// - get_return_object : 코루틴 객체를 반환 (resumable object)
-	// - return_value(val) : co_return val에 의해 호출됨 (코루틴이 영구적으로 끝나지 않으면 없어도 됨)
-	// - return_void() : co_return에 의해 호출됨 (코루틴이 영구적으로 끝나지 않으면 없어도 됨)
-	// - yield_value(val) : co_yield에 의해 호출됨
-	// - initial_suspend() : 코루틴이 실행 전에 중단/연기될 수 있는지
-	// - final_suspend() : 코루틴이 종료 전에 중단/연기될 수 있는지
-	// - unhandled_exception() : 예외 처리시 호출됨
-	struct promise_type
-	{
-		CoroutineObject get_return_object() { return {}; }
-		std::suspend_never initial_suspend() const noexcept { return {}; }
-		std::suspend_never final_suspend() const noexcept { return {}; }
-		void return_void() { }
-		void unhandled_exception() { }
-	};
+	MyInt(int value) : _value(value) {}
+
+	// < = > >= == !=
+	bool operator<(const MyInt& rhs) const { return _value < rhs._value; };
+
+	::strong_ordering operator<=>(const MyInt& rhs) const = default;
+
+	int _value;
 };
 
-CoroutineObject HelloCoroutine()
+struct Strong
 {
+	std::strong_ordering operator<=> (const Strong&) const = default;
+};
 
-	co_return;
-}
-
-Future<int> CreateFuture()
+struct Weak
 {
-	co_return 2021;
-}
+	std::weak_ordering operator<=> (const Weak&) const = default;
+};
 
-Generator<int> GenNumbers(int start = 0, int delta = 1)
+
+struct Partial
 {
-	int now = start;
+	std::partial_ordering operator<=> (const Partial&) const = default;
+};
 
-	while (true)
-	{
-		co_yield now;
-		now += delta;
-	}
-}
-
-Job PrepareJob()
+struct Test
 {
-	// co_await [Awaitable]
-	co_await std::suspend_never();
-}
+	auto operator<=> (const Test&) const = default;
+
+	Strong s;
+	Weak w;
+	Partial p;
+};
 
 
 int main()
 {
-	// 함수가 코루틴이 되리면?
-	// - co_return
-	// - co_yield
-	// - co_await
+	MyInt a(1);
+	MyInt b(2);
 
-	// 코루틴을 사용할 수 있는 Framework를 제공한다. (...)
-	// 3가지 요소로 구성
-	// - promise 객체
-	// - 코루틴 핸들 (밖에서 코루틴을 resume / destroy 할 때 사용. 일종의 리모컨)
-	// - 코루틴 프레임 (promise 객체, 코루틴이 인자 등을 포함하는 heap 할당 객체)
-	// 
-	// co_yield, co_await, co_return을 함수 안에서 사용하면, 그 함수는 코루틴이 됨.
-	/*{
-		Promise prom; (get_return_object)
-		co_await prom.initial_suspend();
-		try
-		{
-			// co_return, co_yield, co_await를 포함하는 코드
-		}
-		catch (...)
-		{
-			prom.unhandled_exception();
-		}
-		co_await prom.final_suspend();
-	}*/
+	bool test = a < b;
+	bool test2 = a > b;
+	bool test3 = a <= b;
 
+	// strong ordering
+	// weak
+	// partial ordering
 
-	// co_await 플로우
-	/*{
-		if awaitable.await_ready() returns false;
-			suspend coroutine
-			awaitable.await_suspend(handle) returns:
-				void:
-					awaitable.await_suspend(handle);
-					coroutine keeps suspended
-					return to caller
-				bool:
-					bool result = awaitable.await_suspend(handle);
-					if (result)
-						coroutine keeps suspended
-						return to caller
-					else
-						return awaitable.await_resume()
-				another coroutine handle:
-					anotherCoroutineHandle = awaitable.await_suspend(handle);
-					anotherCoroutineHandle.resume();
-					return to caller;
-		return awaitable.await_resume();
-	}*/
+	// 비교 특성 (Relational, Equivalence, Compareable)
+	// - 6종 비교 연산 지원 ==, !=, <, <=, >, >= (Relational))
+	// - 모든 Equivalent 한 값들은 서로 구분되지 않고 완전 똑같다. (Equivalence)
+	// ex) 대소문자 구별 안하는 문자열 비교 nero Nero
+	// - a < b, a == b, a > b 중 하나는 참이다. (Compareable)
+	// ex ) float 비교할 때, b가 NaN (not a number)면 3연산이 다 true가 아니다...
+
+	// strong ordering : Relational(o), Equivalence(0), Compareable(0)
+	// weak ordering : Relational(o), Compareable(o)
+	// partial ordering : Relational(O)
+
+	int a1 = 100;
+	int b1 = 200;
 
 
-	auto future = CreateFuture();
+	bool a2 = a1 < b1;
 
-	// TODO : 다른걸 하다.....
+	auto ret = a1 <=> b1;
 
-	cout << future.get() << endl;
+	if (ret < 0)
+		cout << "a < b" << endl;
+	else if (ret == 0)
+		cout << "a == b" << endl;
+	else
+		cout << "a > b" << endl;
 
-	auto numbers = GenNumbers(0, 1);
-	for (int i = 0; i < 20; i++)
-	{
-		numbers.next();
 
-		cout << " " << numbers.get();
-	}
-
-	auto job = PrepareJob();
-
-	job.start();
 }
