09b68c2f71516318d298c23acdbc77c1c155bd35
용주 곽 <amese1225@gmail.com>
2021-12-28 22:36:54 +0900

    Non-Type Template Parameter

--- cpp20/cpp20/cpp20.cpp
+++ cpp20/cpp20/cpp20.cpp
@@ -8,77 +8,78 @@ using namespace std;
 #include <concepts>
 #include <coroutine>
 #include "MyCoroutine.h"
+#include <array>
 
-// 오늘의 주제 : Conditionally Explicit Constructor (조건부 Explicit 생성자)
+// 오늘의 주제 : Non-Type Template Parameter
 
-struct A
+// Non-Type
+// - int, enum
+// - 포인터, 참조값
+// - nullptr_t
+
+// C++20
+// - 부동소수점 (floating-point)
+// - Literal Type (types of constexpr variables)
+// - String Literal
+
+template<double d>
+auto GetDouble()
 {
-	A() = default;
-
-	// 명시적으로
-	explicit A(int n)
-	{
-		cout << n << endl;
-	}
-};
-
-void TestA(A a)
-{
-
+	return d;
 }
 
-struct B
+struct ClassType
 {
-	// explicit : 원하지 않은 형 변환이 일어나지 않도록 제한!
-	template<typename T>
-	explicit B(T n)
-	{
-
-	}
+	constexpr ClassType(int) {}
 };
 
-struct C
+template<int N>
+struct IntToType
 {
-	// 조건부 Explicit
-	// - bool에 의한 간접 변환(Implicit Conversion) 은 허용
-	// - 그 외에는 explicit
-	template<typename T>
-	explicit(!std::is_same<T, bool>::value) C(T t)
-	{
-
-	}
+	enum { value = N };
 };
 
-
-void TestB(B b)
+template<ClassType c>
+auto GetClassType()
 {
-
+	return c;
 }
 
-void TestC(C c)
+template<int N>
+class StringLiteral
 {
+public:
+	constexpr StringLiteral(char const (&str)[N])
+	{
+		::copy(str, str + N, _data);
+	}
 
+	char _data[N];
+};
+
+template<StringLiteral str>
+class ClassTemplate {};
+
+template<StringLiteral str>
+void FunctionTemplate()
+{
+	cout << str._data << endl;
 }
-
 
 int main()
 {
-	A a;
-	TestA(a);
+	auto d1 = GetDouble<5.5>();
 
+	std::array<int, 5> arr;
 
+	auto c1 = GetClassType<ClassType(2021)>();
 
-	//TestA(a2); // 엥?
+	ClassTemplate<"Hello World"> cls1;
+	FunctionTemplate<"Hello World">();
 
-	B b1(2002);
-	TestB(b1);
-	//TestB(1);
-	//TestB(1.f);
+	// 근데 이딴걸 왜씀?
+	// ex) Compile-Time Regular Expression (정규 표현식)
+	// std::regex 같은건 런타임 동작.
+	// 정규 표현식 패턴을 컴파일타임에 안다면, 굳이 런타임까지 갈 필요 없음
 
-	C c1 = true;
-	TestC(c1);
-	TestC(true);
-
-	// 넌 안됨.
-	//TestC(10);
 }
