beaf57d95649d57f19dacfd444dbfb902eacda71
용주 곽 <amese1225@gmail.com>
2021-12-31 23:51:42 +0900

    Container #3 (contains + prefix/suffix  checking)

--- cpp20/cpp20/cpp20.cpp
+++ cpp20/cpp20/cpp20.cpp
@@ -9,39 +9,41 @@ using namespace std;
 #include <coroutine>
 #include "MyCoroutine.h"
 #include <array>
+#include <set>
 
-// 오늘의 주제 : Container #1
-// (erase, erase_if)
+// 오늘의 주제 : Container #3
+// (contains + prefix/suffix  checking)
 
 int main()
 {
-	vector<int> vec{ -1,2,-3,4,-5 };
+	std::set s{ 1,2,3,4,5 };
 
-	/*for (auto it = vec.begin(); it != vec.end();)
+	auto findIt = s.find(2);
+	if (findIt != s.end())
 	{
-		int value = *it;
-		if (value < 0)
-		{
-			it = vec.erase(it);
-		}
-		else
-		{
-			it++;
-		}
-	}*/
+		cout << "잡았다 요놈" << endl;
+	}
 
-	// 슈퍼 함정 카드
-	// -1 2 -3 4 -5
-	//  2 4 -3 4 -5
-	/*auto newEnd = std::remove_if(vec.begin(), vec.end(), [](int num) {return num < 0; });
-	vec.erase(newEnd, vec.end());*/
+	if (s.contains(3))
+	{
+		cout << "잡았다 요놈" << endl;
+	}
 
-	// C++20 erase, erase_if
-	//std::erase(vec, 2);
-	std::erase_if(vec, [](int num) {return num < 0; });
+	std::map<int, int>m{ {1, 100}, {2, 20000} };
 
-	// C++의 엄청난 함정
-	for (auto num : vec)
-		cout << num << endl;
+	if (m.contains(1))
+	{
+		cout << "잡았다 요놈" << endl;
+	}
+
+	// string의 prefix/suffix (starts_with, ends_with)
+	std::string str = "Hello World";
+
+	bool b1 = str.starts_with("Hel");
+	bool b2 = str.ends_with("rld");
+
+	cout << b1 << endl;
+	cout << b2 << endl;
+
 
 }
