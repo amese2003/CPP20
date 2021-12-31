8b0efdf86bedba29af56b264fa4088de295297df
용주 곽 <amese1225@gmail.com>
2021-12-31 23:44:29 +0900

    Container #2 (erase, erase_if)

--- cpp20/cpp20/cpp20.cpp
+++ cpp20/cpp20/cpp20.cpp
@@ -11,61 +11,37 @@ using namespace std;
 #include <array>
 
 // 오늘의 주제 : Container #1
-
-// 1. vector, string 등에 constexpr 사용 가능
-// algorithm 100개 이상의 함수들이 constexpr로 바뀜
-
-constexpr int Test()
-{
-	std::vector<int> v{ 1,4,5,2,3 };
-	std::sort(v.begin(), v.end());
-	return v.back();
-}
-
-// 2. std::array 만드는 방법이 추가됨 (std::to_array)
-
-// 3. std::make_shared로 shared_ptr 생성 가능
-
-template<typename T>
-struct ArrayDeleter
-{
-	void operator()(const T* ptr)
-	{
-		delete[] ptr;
-	}
-};
+// (erase, erase_if)
 
 int main()
 {
-	constexpr int ret = Test();
+	vector<int> vec{ -1,2,-3,4,-5 };
 
-	enum
+	/*for (auto it = vec.begin(); it != vec.end();)
 	{
-		VALUE = ret
-	};
+		int value = *it;
+		if (value < 0)
+		{
+			it = vec.erase(it);
+		}
+		else
+		{
+			it++;
+		}
+	}*/
 
+	// 슈퍼 함정 카드
+	// -1 2 -3 4 -5
+	//  2 4 -3 4 -5
+	/*auto newEnd = std::remove_if(vec.begin(), vec.end(), [](int num) {return num < 0; });
+	vec.erase(newEnd, vec.end());*/
 
-	auto arr1 = std::to_array("Hello World");
-	for (auto ch : arr1)
-		cout << ch;
+	// C++20 erase, erase_if
+	//std::erase(vec, 2);
+	std::erase_if(vec, [](int num) {return num < 0; });
 
-	cout << endl;
+	// C++의 엄청난 함정
+	for (auto num : vec)
+		cout << num << endl;
 
-	auto arr2 = std::to_array<int>({1,2,3,4,5});
-	for (auto ch : arr2)
-		cout << ch;
-
-	cout << endl;
-
-
-	
-	/*int* arr3 = new int[3];
-	delete[] arr3;*/
-	// C++ 11
-	std::shared_ptr<int> sp(new int[10], ArrayDeleter<int>());
-
-	// C++ 20
-	std::shared_ptr<double[]> shared_arr = std::make_shared<double[]>(1024);
-
-	shared_arr[1] = 1.0;
 }
