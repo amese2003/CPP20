c669b7857e911b7a50847c31b46c1a199a3df416
용주 곽 <amese1225@gmail.com>
2022-01-02 17:05:11 +0900

    기타 함수 (source_location, bind_front, is_constant_evaluated)

--- cpp20/cpp20/cpp20.cpp
+++ cpp20/cpp20/cpp20.cpp
@@ -16,101 +16,61 @@ using namespace std;
 #include <bitset>
 #include <chrono>
 #include <format>
+#include <source_location>
+#include <functional>
 
-// 오늘의 주제 : Format
+// 오늘의 주제 : 기타 함수
+
+void PrintLog(const char* filename, int line)
+{
+	cout << format("{} Line({})", filename, line) << endl;
+}
+
+int Add(int a, int b)
+{
+	return a + b;
+}
+
+void Test()
+{
+	if (std::is_constant_evaluated())
+	{
+		// 컴파일
+	}
+	else
+	{
+		// 런타임
+	}
+}
 
 int main()
 {
-	//printf("%d", 10);
+	// 로그 남길 때
+	// 멀티스레드 환경에서 데드락 탐지
+	// 메모리 누수를 잡기 위해서 할당/해제
+	// source_location
+	PrintLog(__FILE__, __LINE__);
 
-	// format		: 포맷 스트링
-	// format_to	: 결과를 output iterator에 반환
-	// format_to_n	: 최대 n개 문자를 output iterator에 반환
-
-	int value = 20;
-	cout << format("Hello C++{}", value) << endl;
-
-	string buffer;
-	format_to(std::back_inserter(buffer), "Hello C++{}", 20);
-	cout << buffer << endl;
-
-	buffer.clear();
-	format_to_n(std::back_inserter(buffer), 5, "Hello C++{}", 20);
-	cout << buffer << endl;
-
-	// 진짜로 { 쓰고싶으면? {{ 두개를 사용하면 됨.
-	cout << format("{{Hello C++{}}}", value) << endl;
+	auto src = std::source_location::current();
+	PrintLog(src.file_name(), src.line());
 
 
-	cout << format("{} {} {}", 10, 20, 30) << endl;
-	cout << format("{0} {2} {1}", 10, 20, 30) << endl;
+	// bind_front
+	//Add(10, 20);
+	using namespace std::placeholders;
+	auto func = std::bind(Add, 10, _1);
+	cout << func(20) << endl;
 
-	// Fill Character (빈칸 채우는 문자 지정 가느으. 기본값은 space)
-	// Alignment
-	// < left (숫자가 아니라면 얘가 default
-	// > right (숫자라면 얘가 default)
-	// ^ center
+	auto func2 = std::bind_front(Add, 10);
+	cout << func2(20) << endl;
 
-	int num = 100;
-	cout << format("{:10}", num) << endl;
-	cout << format("{:10}", 'x') << endl;
-	cout << format("{:*<10}", 'x') << endl;
-	cout << format("{:*>10}", 'x') << endl;
-	cout << format("{:*^10}", 'x') << endl;
+	auto func3 = [](int b) { return Add(10, b); };
+	cout << func3(20) << endl;
 
-	// 부호
-	// 정수나 부동소수점에 사용
-	// + 양수
-	// - 음수 << default
+	// is_constant_evaluated
+	// consteval 붙은 함수는 컴파일 타임에 실행됨
+	// constexpr 붙은 함수는 컴파일/런타임 둘다 됨
+	// 보통 함수 런타임
 
-	cout << format("{0:}, {0:+}, {0:-}, {0: }", 0) << endl;
-	cout << format("{0:}, {0:+}, {0:-}, {0: }", 1) << endl;
-	cout << format("{0:}, {0:+}, {0:-}, {0: }", -1) << endl;
-
-	// [#]
-	// 정수에 대해, 0b, 0, 0x 이진, 팔진, 십육진
-	// 0 : pad with leading zeros
-
-	cout << format("{:#015}", 0x78) << endl;
-	cout << format("{:#015b}", 0x78) << endl;
-	cout << format("{:015x}", 0x78) << endl;
-
-	// [width / precision]
-	// width : {:n} n은 minimum width
-	// precision : .에다가 양수 조합 (string이라면 문자 개수, float라면 formatting precision)
-	int i = 123456789;
-	double d = 123.456789;
-
-	cout << "---" << format("{}", i) << "---" << endl;
-	cout << "---" << format("{:15}", i) << "---" << endl; // w = 15
-	cout << "---" << format("{}", d) << "---" << endl;
-	cout << "---" << format("{:15}", d) << "---" << endl; // w = 15
-
-	cout << "---" << format("{:.100}", d) << "---" << endl; // p = 50
-	cout << "---" << format("{:.8}", d) << "---" << endl;
-	cout << "---" << format("{:.5}", d) << "---" << endl;
-	cout << "---" << format("{:.5}", "Hello World") << "---" << endl;
-
-	// [Type]
-	// 대부분 컴파일러가 알아서 어떤 타입인지 추론은 해줌.
-	// 근데 우리가 지정할수도 있다.
-	// s : string
-	// b : binary
-	// B : b와 같지만 OB로 시작
-	// d : decimal
-	// o : octal
-	// x : hexadecimal
-	// X : x와 같지만  OX로 시작
-	// 부동소수점 관련 (e, E, f, F, g, G) : 생략 (지수 표시, 정밀도 등)
-
-	cout << format("{:}", num) << endl;
-	cout << format("{:d}", num) << endl;
-	cout << format("{:b}", num) << endl;
-	cout << format("{:o}", num) << endl;
-	cout << format("{:x}", num) << endl;
-
-	// [User Define Types]
-	// std::formatter 클래스를 신규 타입에 대해 특수화
-	// - 멤버 함수 parse, format을 구현해야함
-	// -- 근데 아직 버그가 있다! C2039 에러
+	
 }
