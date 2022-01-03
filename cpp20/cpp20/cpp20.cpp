995c0f0059d32683b5fff870156d32afc2b99b50
용주 곽 <amese1225@gmail.com>
2022-01-03 23:14:24 +0900

    semaphore

--- cpp20/cpp20/cpp20.cpp
+++ cpp20/cpp20/cpp20.cpp
@@ -20,61 +20,59 @@ using namespace std;
 #include <functional>
 #include <atomic>
 #include <thread>
+#include <semaphore>
 
-// 오늘의 주제 : atomic
+// 오늘의 주제 : semaphore
 
+counting_semaphore<1> sem(0);
+vector<int> vec;
+
+void PrepareWork()
+{
+	vec.insert(vec.end(), { 1,2,3,4,5 });
+
+	this_thread::sleep_for(1s);
+
+	cout << "데이터 준비됨." << endl;
+	sem.release();
+}
+
+void CompleteWork()
+{
+	cout << "데이터 기다리는중..." << endl;
+	sem.acquire();
+
+	cout << "데이터 출력중" << endl;
+
+	for (auto n : vec)
+		cout << n << endl;
+}
 
 int main()
 {
-	// atomic_ref
-	int value = 0;
+	// mutex
+	// 화장실 키
+	// [키]
 
-	//int& ref = value;
-	atomic_ref<int> ref(value);
+	// semaphore
+	// 화장실
+	// [키][키][키]
 
-	thread t1([&ref]() {
+	//counting_semaphore<3>;
+	//binary_semaphore = counting_semaphore<1>;
 
-		for (int i = 0; i < 1000000; i++)
-			ref++;
-		});
+	// max(); // 최대 카운터값
+	// acquire(); // counter를 1만큼 감소 (counter = 0 이면 양수 될 때 까지 대기)
+	// release(upd = 1); // counter를 upd만큼 증가 (대기하던 스레드가 있으면 풀어줌)
+	// try_acquire(); // 카운터가 0보다 크면, 1감소
+	// try_acquire_for(relTime); // 최대 relTime만큼 counter를 1 감소하려 시도
+	// try_acquire_until(absTime); // 최대 absTime까지 counter를 1 감소하려 시도
 
-	thread t2([&ref]() {
-
-		for (int i = 0; i < 1000000; i++)
-			ref--;
-		});
+	thread t1(PrepareWork);
+	thread t2(CompleteWork);
 
 	t1.join();
 	t2.join();
 
-	cout << ref << endl;
-	// atomic shared_ptr
-
-	// shared_ptr
-
-	// [resource(data)][control block]
-	// Thread-Safe?
-	// - control block은 thread-safe한게 맞다.
-
-
-	class User
-	{
-	public:
-		int id = 0;
-		int hp = 0;
-	};
-
-	shared_ptr<User> targetUser = make_shared<User>();
-
-	targetUser = make_shared<User>();
-
-
-	targetUser = make_shared<User>();
-
-	// atomic_shared_ptr 과 atomic_weak_ptr이 제안 되었음
-	// atomic<shared_ptr<T>>, atomic<weak_ptr<T>>
-	atomic<shared_ptr<User>> atomicSharedPtr = make_shared<User>();
-
-	// LockFree 프로그래밍 유용
-	// 일상에선 사용할까?
+	return 0;
 }
